f4d6fae7d60e17c3098eabf49fb00434d847a1cd
viktor <vozbrannyjv@gmail.com>
2018-11-28 21:40:25 +0300

    com_Github

---
+++ src/js/Github.js
@@ -0,0 +1,8 @@
+
+
+    class Github {
+        constructor(){
+            this.client_id = 'Iv1.0f065558fc754553';
+            this.client_secret = 'ebea76b2cbb6c1c21c8bd3b40ea87870312afd98';
+        }
+    }
